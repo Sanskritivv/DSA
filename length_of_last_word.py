e636cf8a16ccfdd2fc8b2f0b583ef9f2dd0cdc34
Sanskriti Verma <136150178+Sanskritivv@users.noreply.github.com>
2024-08-13 23:40:25 +0530

    Create length_of_last_word.py

---
+++ length_of_last_word.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        word=s.strip().split()
+        return len(word[-1])
